4c1d8fc5427723a4bf8a1aca1c323ed1d6900139
NaoyaNomura <2002nomuranaoya@gmail.com>
2025-12-15 10:21:50 +0900

    purokisi ga doutara

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from werkzeug.middleware.proxy_fix import ProxyFix # 【追加】これが必要
 from .config import Config
 from .extensions import db, csrf
 
@@ -8,6 +9,11 @@ def create_app():
     
     # 2. 設定ファイル（config.py）の内容を読み込む
     app.config.from_object(Config)
+
+
+# 【追加】Render（プロキシ環境）対策
+    # これがないと、IPアドレスが取れなかったり、httpsへのリダイレクトがおかしくなります
+    app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
 
     # 3. 拡張機能（DBやCSRF）をアプリと紐付ける
     # これにより、extensions.py で作った空の箱に中身が入ります
